1182d9a28f348c2713868fd42e982d40094294c9
Leeyoungji1223 <youngji1223@naver.com>
2022-08-12 10:57:34 +0900

    2022-08-12

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
 $(document).ready(function(){
-    $('.slider').slick({
-      dots: true,
-    });
+  $('.slider').slick({
+    dots: true,
   });
+});
+
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
